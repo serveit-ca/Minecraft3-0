d5d2c6ac5d8d19a38b950f0ec6c80781b71750a4
Nathan Tymos <nathan@serveit.ca>
2022-02-04 10:48:42 -0800

    Update start.js

--- start.js
+++ start.js
@@ -2,3 +2,27 @@
  * This is a comment
  */
 player.say(":)")
+
+// This is the first piece of code i am writing in term 3
+
+/* 
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+*/
+
+let text;
+
+text = "A really big Jump";
+player.say(text);
+
+text = "A Really Small Jump ";
+
+player.say(text);
+
+text += "A Really Long Jump";
+
+player.say(text);
